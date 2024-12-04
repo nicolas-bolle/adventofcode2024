7685396c9f51d81d6ed7df7d3061f58d8731814b
Nicolas Bolle <nicolasbolle@gmail.com>
2024-12-04 12:54:18 -0500

    day 4

---
+++ solutions/nickb_day4.py
@@ -0,0 +1,124 @@
+"""Day 4"""
+
+
+# pylint: disable=unused-import, invalid-name, redefined-outer-name
+
+from abc import ABC, abstractmethod
+from functools import lru_cache
+
+import re
+from collections import deque
+import numpy as np
+import pandas as pd
+
+from utils.inputs import (
+    get_input,
+    split,
+    split_newline,
+    split_lax,
+    list_map,
+    list_reshape,
+    get_int,
+    get_float,
+)
+
+DAY = 4
+
+
+def solution_part1(s: str):
+    """Part 1 solution from the plaintext input
+    I manually coded in the 8 relative positions (horizontal, vertical, diagonal) that an 'XMAS' could appear in
+    The code (inefficiently) loops through all shifts of these to look for 'XMAS'es
+    """
+    A = np.array(list_map(list, split_newline(s)))
+
+    c = 0
+
+    idxs = [
+        ([0, 0, 0, 0], [0, 1, 2, 3]),
+        ([0, 0, 0, 0], [0, -1, -2, -3]),
+        ([0, 1, 2, 3], [0, 0, 0, 0]),
+        ([0, -1, -2, -3], [0, 0, 0, 0]),
+        ([0, 1, 2, 3], [0, 1, 2, 3]),
+        ([0, 1, 2, 3], [0, -1, -2, -3]),
+        ([0, -1, -2, -3], [0, 1, 2, 3]),
+        ([0, -1, -2, -3], [0, -1, -2, -3]),
+    ]
+
+    padding = 5
+    n = len(A)
+    for idx_x, idx_y in idxs:
+        for offset_x in range(-padding, n + padding + 1):
+            for offset_y in range(-padding, n + padding + 1):
+                _idx_x = np.array(idx_x) + offset_x
+                _idx_y = np.array(idx_y) + offset_y
+
+                if (
+                    min(_idx_x) < 0
+                    or min(_idx_y) < 0
+                    or max(_idx_x) >= n
+                    or max(_idx_y) >= n
+                ):
+                    continue
+
+                text = "".join(A[_idx_x, _idx_y])
+                if text == "XMAS":
+                    c = c + 1
+
+    return c
+
+
+def solution_part2(s: str):
+    """Part 2 solution from the plaintext input
+    For this part we just need one "pattern", and we can match it to 4 'X-MAS'es
+    The 4 correspond to the 4 rotations of the X
+    """
+    A = np.array(list_map(list, split_newline(s)))
+
+    c = 0
+
+    padding = 4
+    n = len(A)
+
+    idx_x = [0, -1, -1, 1, 1]
+    idx_y = [0, -1, 1, 1, -1]
+
+    matches = [
+        "AMMSS",
+        "AMSSM",
+        "ASSMM",
+        "ASMMS",
+    ]
+
+    for offset_x in range(-padding, n + padding + 1):
+        for offset_y in range(-padding, n + padding + 1):
+            _idx_x = np.array(idx_x) + offset_x
+            _idx_y = np.array(idx_y) + offset_y
+
+            if (
+                min(_idx_x) < 0
+                or min(_idx_y) < 0
+                or max(_idx_x) >= n
+                or max(_idx_y) >= n
+            ):
+                continue
+
+            text = "".join(A[_idx_x, _idx_y])
+            if text in matches:
+                c = c + 1
+
+    return c
+
+
+if __name__ == "__main__":
+    s = get_input(DAY)
+    print()
+    soln1 = solution_part1(s)
+    print("Part 1 solution:")
+    print(soln1)
+    print()
+    soln2 = solution_part2(s)
+    print("Part 2 solution:")
+    print(soln2)
+    print()
+    print("Done")
